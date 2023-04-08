ff2ff6146f693704ce855883dc29039ee120695a
AnasAlkhamis <anas.alkhamnis@gmail.com>
2023-04-08 16:14:29 +0300

    from server

---
+++ controllers/user.js
@@ -0,0 +1,15 @@
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
